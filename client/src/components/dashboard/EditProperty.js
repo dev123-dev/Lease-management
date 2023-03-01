330e8d6611068c22c4a700671a4474d1580a76ca
Shreya <dev@gmail.com>
2023-03-01 10:11:44 +0530

    3 files changed by shreya

--- client/src/components/dashboard/EditProperty.js
+++ client/src/components/dashboard/EditProperty.js
@@ -15,7 +15,7 @@ const EditProperty = ({
   Property,
   updateProperty,
 }) => {
-  console.log("this is property details",Property,user)
+  console.log("this is property details", Property, user);
   const [showEditModal, setShowEditModal] = useState(false);
   const handleEditModalClose = () => setShowEditModal(false);
   const handleOpen = () => setShowEditModal(true);
@@ -39,7 +39,6 @@ const EditProperty = ({
     setitem(delitem);
   };
 
-
   const [doornum, setdoornum] = useState("");
   const [dno, setdno] = useState(Property.shopDoorNo);
 
@@ -62,8 +61,8 @@ const EditProperty = ({
   const [formData, setFormData] = useState({
     buildingName: Property.buildingName,
     shopDoorNo: [],
-    Location : Property.Location,
-    shopAddress : Property.shopAddress,
+    Location: Property.Location,
+    shopAddress: Property.shopAddress,
     hikePercentage: Property.hikePercentage,
     stampDuty: Property.stampDuty,
     LeaseTime: Property.leaseTimePeriod,
@@ -87,130 +86,153 @@ const EditProperty = ({
 
   const onUpdate = () => {
     const update = {
-      OrganizationName : user.OrganizationName,
-      Orgainzation_id : user.OrganizationId,
-      Property_id : Property._id,
+      OrganizationName: user.OrganizationName,
+      Orgainzation_id: user.OrganizationId,
+      Property_id: Property._id,
       buildingName: buildingName,
       shopDoorNo: dno,
-      shopAddress : shopAddress,
+      shopAddress: shopAddress,
       hikePercentage: hikePercentage,
       stampDuty: stampDuty,
       leaseTimePeriod: LeaseTime,
-      Location : Location,
+      Location: Location,
       isSubmitted: true,
       shopStatus: "Acquired",
     };
-console.log("updated data in main page",update)
-updateProperty(update);
+    console.log("updated data in main page", update);
+    updateProperty(update);
   };
 
   return (
-    <Fragment >
-            <div className="col-lg-6">
-              <label>
-                BuildingName
-                <i className="text-danger ">
-                  <b>*</b>
-                </i>
-              </label>
+    <Fragment>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6">
+            <label>
+              BuildingName
+              <i className="text-danger ">
+                <b>*</b>
+              </i>
+            </label>
 
-              <input
-                type="text"
-                placeholder="BuildingName"
-                name="buildingName"
-                value={buildingName}
-                className="form-control input"
-                 onChange={(e) => onInputChange(e)}
-                required
-              />
-              <br></br>
-            </div>
-            <div className="col-lg-6">
-              <label>
-                StampDuty{" "}
-                <i className="text-danger ">
-                  <b>*</b>
-                </i>
-              </label>
-              <input
-                type="text"
-                placeholder="StampDuty"
-                name="stampDuty"
-                value={stampDuty}
-                className="form-control  input"
-                onChange={(e) => onInputChange(e)}               
-                 required
-              />
-            </div>
-            <div className="col-lg-6">
-              <label>
-                Hike<b>%</b>{" "}
-                <i className="text-danger ">
-                  <b>*</b>
-                </i>
-              </label>
-              <input
-                type="text"
-                placeholder="HikePercent"
-                name="hikePercentage"
-                value={hikePercentage}
-                className="form-control  input"
-                 onChange={(e) => onInputChange(e)}
-                required
-              />
-            </div>
-            <div className="col-lg-6">
-              <label>
-                LeaseTimePeriod{" "}
-                <i className="text-danger ">
-                  <b>*</b>
-                </i>
-              </label>
-              <textarea
-                name="LeaseTime"
-                id=" addprop "
-                value={LeaseTime}
-                className="textarea form-control"
-                rows="4"
-                placeholder="Time"
-                 onChange={(e) => onInputChange(e)}
-                style={{ width: "100%" }}
-                required
-              ></textarea>
-             
-            </div>
-            <div className="col-lg-6">
-              <label>
-                Address
-                <i className="text-danger ">
-                  <b>*</b>
-                </i>
-              </label>
+            <input
+              type="text"
+              placeholder="BuildingName"
+              name="buildingName"
+              value={buildingName}
+              className="form-control input"
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+            <br></br>
+          </div>
+          <div className="col-lg-6">
+            <label>
+              StampDuty{" "}
+              <i className="text-danger ">
+                <b>*</b>
+              </i>
+            </label>
+            <input
+              type="text"
+              placeholder="StampDuty"
+              name="stampDuty"
+              value={stampDuty}
+              className="form-control  input"
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+          </div>
+          <div className="col-lg-6">
+            <label>
+              Hike<b>%</b>{" "}
+              <i className="text-danger ">
+                <b>*</b>
+              </i>
+            </label>
+            <input
+              type="text"
+              placeholder="HikePercent"
+              name="hikePercentage"
+              value={hikePercentage}
+              className="form-control  input"
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+          </div>
+          <div className="col-lg-6">
+            <label>
+              LeaseTimePeriod{" "}
+              <i className="text-danger ">
+                <b>*</b>
+              </i>
+            </label>
+            <input
+              type="text"
+              placeholder="TimePeriod"
+              name="hikePercentage"
+              value={LeaseTime}
+              className="form-control  input"
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+          </div>
+          <div className="col-lg-6">
+            <label>
+              Address
+              <i className="text-danger ">
+                <b>*</b>
+              </i>
+            </label>
 
-              <textarea
-                name="shopAddress"
-                value={shopAddress}
-                id=" addprop "
-                className="textarea form-control"
-                rows="4"
-                placeholder="Address"
-                 onChange={(e) => onInputChange(e)}
-                style={{ width: "100%" }}
-                required
-              ></textarea>
-              <br></br>
-            </div>
+            <textarea
+              name="shopAddress"
+              value={shopAddress}
+              id=" addprop "
+              className="textarea form-control"
+              rows="4"
+              placeholder="Address"
+              onChange={(e) => onInputChange(e)}
+              style={{ width: "100%" }}
+              required
+            ></textarea>
+            <br></br>
+          </div>
 
-
-            <div className="  col-lg-6 ">
-              <label className="ml-2">
-                DoorNo{" "}
-                <i className="text-danger  ">
-                  <b>*</b>
-                </i>
-              </label>
+          <div className="  col-lg-6 ">
+            <label className="ml-2">
+              DoorNo{" "}
+              <i className="text-danger  ">
+                <b>*</b>
+              </i>
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="shopDoorNo"
+              value={inputdata}
+              onChange={(e) => setinput(e.target.value)}
+              placeholder="Door Number"
+              id="Door Number"
+            ></input>
+            <div>
+              <div className="locadd " onClick={addItem}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-plus-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                  />
+                </svg>
+              </div>
               <div className="showItemcl ">
-                 {dno.map((ele, index1) => {
+                {dno.map((ele, index1) => {
                   return (
                     <div className="eachItem" key={index1}>
                       <span>{ele}</span>{" "}
@@ -222,11 +244,13 @@ updateProperty(update);
                       </button>
                     </div>
                   );
-                })} 
+                })}
               </div>
+            </div>
+          </div>
 
-            <div className="col-lg-6">
-            <label className="ml-2">
+          <div className="col-lg-6">
+            <label className="">
               Location
               <i className="text-danger  ">
                 <b>*</b>
@@ -234,25 +258,26 @@ updateProperty(update);
               :
             </label>
             <input
-                type="text"
-                name="Location"
-                value={Location}
-                className="form-control  input"
-                 onChange={(e) => onInputChange(e)}
-                required
-              />
-            </div>
-            <div className="col-lg-12">
-              <button
-                className="btn sub_form btn_continue Save float-right  text-end"
-                id="savebtn"
-                 onClick={() => onUpdate()}
-              >
-                Save
-              </button>
-            </div>
+              type="text"
+              placeholder="Location"
+              name="Location"
+              value={Location}
+              className="form-control  input"
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+          </div>
+          <div className="col-lg-12">
+            <button
+              className="btn sub_form btn_continue Save float-right  text-end"
+              id="savebtn"
+              onClick={() => onUpdate()}
+            >
+              Save
+            </button>
+          </div>
         </div>
- 
+      </div>
 
       <Modal
         // show={showInformationModal}
@@ -278,18 +303,18 @@ updateProperty(update);
         </Modal.Footer>
       </Modal>
     </Fragment>
-  )
+  );
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
   tenants1: state.tenants,
-  tenants : state.tenants,
+  tenants: state.tenants,
 });
 
 export default connect(mapStateToProps, {
   // UpdateTenantsDetails,
   // getAllTenants,
   // tenantsDetailsHistory,
-   updateProperty,
+  updateProperty,
 })(EditProperty);
