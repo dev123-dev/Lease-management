65fedc2938dbb799e1d794a1bc33788ddafcbbcf
Rakxit-shetty <folder@gmail.com>
2023-04-01 14:30:47 +0530

    doorno + and - done by shreya

--- client/src/components/dashboard/AddTenantDetails.js
+++ client/src/components/dashboard/AddTenantDetails.js
@@ -723,8 +723,10 @@ const AddTenantDetails = ({
                               onClick={() => onSelectChange(DoorNumber)}
                             >
                               {DoorNumber.value}
-                              <span  className="mx-2">
-                                <b>+</b>
+                              <span  className="ml-4">
+                                <b className="text-dark "><svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+</svg></b>
                               </span>
                             </button>
                           );
@@ -752,8 +754,10 @@ const AddTenantDetails = ({
                               onClick={() => onRemoveChange(Doornumber)}
                             >
                               {Doornumber.value}
-                              <span  className="mx-2">
-                               <b>-</b> 
+                              <span  className="ml-4">
+                               <b className="text-light"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-dash-square-fill" viewBox="0 0 16 16">
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z"/>
+</svg></b> 
                               </span>
                             </button>
                           );
--- client/src/components/dashboard/EditTenantDetails.js
+++ client/src/components/dashboard/EditTenantDetails.js
@@ -749,9 +749,11 @@ const EditTenantDetails = ({
                           onClick={() => onSelectChange(DoorNumber)}
                         >
                           {DoorNumber.doorNo}
-                          <span id="savebtn" className="mx-2">
-                            +
-                          </span>
+                          <span  className="ml-4">
+                                <b className="text-dark "><svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+</svg></b>
+                              </span>
                         </button>
                       );
                     })}
@@ -782,9 +784,11 @@ const EditTenantDetails = ({
                         >
                           {Doornumber.value}
 
-                          <span id="savebtn" className="mx-2">
-                            -
-                          </span>
+                          <span  className="ml-4">
+                               <b className="text-light"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-dash-square-fill" viewBox="0 0 16 16">
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z"/>
+</svg></b> 
+                              </span>
                         </button>
                         // </p>
                       );
