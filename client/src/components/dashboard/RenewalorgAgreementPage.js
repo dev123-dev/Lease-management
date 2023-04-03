5c593b7403580c0e918511b216b3010f204144c4
devteam 01 <devteam01@pinnacle.com>
2023-04-03 12:05:29 +0530

    done date 2nd

--- client/src/components/dashboard/RenewalorgAgreementPage.js
+++ client/src/components/dashboard/RenewalorgAgreementPage.js
@@ -15,7 +15,7 @@ const ReneworgAggreement = ({
   const orgData = Location.state;
   const histroy = useHistory();
   //formData
- 
+
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth() + 1;
@@ -61,11 +61,8 @@ const ReneworgAggreement = ({
 
   const [showModal, setShowModal] = useState(false);
 
-  
   const onSubmit = (e) => {
-
     e.preventDefault();
-
 
     const finalData = {
       isSubmitted: true,
@@ -78,104 +75,94 @@ const ReneworgAggreement = ({
       date: entryDate,
       enddate: leaseEndDate,
     };
-//console.log("final data ",finalData)
+    //console.log("final data ",finalData)
     RenewOrgDetailsform(finalData);
-   setShowModal(true)
+    setShowModal(true);
   };
 
   return !isAuthenticated || !user || !users ? (
     <Fragment></Fragment>
   ) : (
     <Fragment>
-     
-<div className="mt-5">
-<div className="col-lg-6 col-md-12 col-sm-12 col-12 py-3">
-      <div className="row container-fluid mt-5 cardAgreement"  style={{backgroundColor:"#e0e9e5f5"}}>
-      <form onSubmit={(e) => onSubmit(e)}>
-      <div className="col-lg-12 col-sm-12 col-md-12 col-12">
-              <span
-                style={{ fontFamily: "Serif", color: "#095a4a", }}
-                className="font-weight-bold headsize h2"
-              >
-                Renewal Agreement
-              </span>
-            </div>
-        <div className="row col-lg-12 col-md-9 col-sm-9 col-12 py-3">
-          <div className="col-lg-4 col-md-2 col-sm-1 col-12">
-            <label>
-              {" "}
-              Organization Name*:{" "}
-              
-            </label>
-          </div>
+      <div className="mt-5">
+        <div className="col-lg-6 col-md-12 col-sm-12 col-12 py-3">
+          <div
+            className="row container-fluid mt-5 cardAgreement"
+            style={{ backgroundColor: "#e0e9e5f5" }}
+          >
+            <form onSubmit={(e) => onSubmit(e)}>
+              <div className="col-lg-12 col-sm-12 col-md-12 col-12">
+                <span
+                  style={{ fontFamily: "Serif", color: "#095a4a" }}
+                  className="font-weight-bold headsize h2"
+                >
+                  Renewal Agreement
+                </span>
+              </div>
+              <div className="row col-lg-12 col-md-9 col-sm-9 col-12 py-3">
+                <div className="col-lg-4 col-md-2 col-sm-1 col-12">
+                  <label> Organization Name*: </label>
+                </div>
 
-          <div className="col-lg-5  col-md-4 col-sm-4 col-12">
-          <label><b>{orgData && orgData.OrganizationName}</b></label>
-          </div>
-        </div>
-        <div className="row col-lg-12 col-md-9 col-sm-9 col-12 py-3">
-          <div className="col-lg-4 col-md-2 col-sm-4 col-12">
-            <label>
-            Lease Start Date* :
-              
-            </label>
-          </div>
+                <div className="col-lg-5  col-md-4 col-sm-4 col-12">
+                  <label>
+                    <b>{orgData && orgData.OrganizationName}</b>
+                  </label>
+                </div>
+              </div>
+              <div className="row col-lg-12 col-md-9 col-sm-9 col-12 py-3">
+                <div className="col-lg-4 col-md-2 col-sm-4 col-12">
+                  <label>Lease Start Date* :</label>
+                </div>
 
-          <div className="col-lg-5  col-md-4 col-sm-4 col-12">
-          <input
-              type="date"
-              placeholder="dd/mm/yyyy"
-              className="form-control cpp-input datevalidation"
-              name="tenantLeaseStartDate"
-              value={entryDate}
-              onChange={(e) => onDateChangeEntry(e)}
-              required
-            />
-          </div>
-        </div>
-        <div className="row col-lg-12 col-md-9 col-sm-9 col-12 py-3">
-          <div className="col-lg-4 col-md-2 col-sm-4 col-12">
-            <label>
-              Lease End Date*:{" "}
-              
-            </label>
-          </div>
+                <div className="col-lg-5  col-md-4 col-sm-4 col-12">
+                  <input
+                    type="date"
+                    className="form-control cpp-input datevalidation"
+                    name="tenantLeaseStartDate"
+                    value={entryDate}
+                    onChange={(e) => onDateChangeEntry(e)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="row col-lg-12 col-md-9 col-sm-9 col-12 py-3">
+                <div className="col-lg-4 col-md-2 col-sm-4 col-12">
+                  <label>Lease End Date*: </label>
+                </div>
 
-          <div className="col-lg-5  col-md-4 col-sm-4 col-12">
-          <input
-              className="form-control cpp-input datevalidation"
-              placeholder="dd-mm-yyyy"
-              value={leaseEndDate}
-             required
-            ></input>
+                <div className="col-lg-5  col-md-4 col-sm-4 col-12">
+                  <input
+                    className="form-control cpp-input datevalidation"
+                    value={leaseEndDate}
+                    required
+                  ></input>
+                </div>
+              </div>
+              <div className="col-lg-12 Savebutton" size="lg">
+                <button
+                  variant="success"
+                  id="buttonchanges"
+                  className="float-right mb-2"
+                  type="submit"
+                >
+                  <b>Renew</b>
+                </button>
+                <button
+                  className="float-right mb-2"
+                  type="button"
+                  variant="success"
+                  id="buttonchanges"
+                  onClick={() => {
+                    histroy.push("/MainSuper");
+                  }}
+                >
+                  <b>Cancel</b>
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        <div className="col-lg-12 Savebutton" size="lg">
-        
-        <button
-                    variant="success"
-                    id="buttonchanges"
-                    className="float-right mb-2"
-                    type="submit"
-                   
-                  >
-                    <b>Renew</b>
-                  </button>
-            <button
-             className="float-right mb-2"
-                    type="button"
-                    variant="success"
-                    id="buttonchanges"
-                    onClick={() => { histroy.push('/MainSuper') }}
-                  >
-                    <b>Cancel</b>
-                  </button>
-           
-           
-        </div>
-        </form>
-      </div>
-      </div>
       </div>
       <Modal
         show={showModal}
@@ -185,13 +172,22 @@ const ReneworgAggreement = ({
         centered
       >
         <Modal.Header className="confirmbox-heading">
-                        <h4 className="mt-0 mr-5 text-center">
-                         CONFIRMATION&nbsp;&nbsp;
-                        </h4>
-                      </Modal.Header>
-      <ModalBody className="h3 text-center">Renewed Successfully..!!</ModalBody>
-       <ModalFooter><button  id="buttonchanges"
-                    className="float-right mb-2"  onClick={() => { histroy.push('/SuperUser') }}>OK</button></ModalFooter>
+          <h4 className="mt-0 mr-5 text-center">CONFIRMATION&nbsp;&nbsp;</h4>
+        </Modal.Header>
+        <ModalBody className="h3 text-center">
+          Renewed Successfully..!!
+        </ModalBody>
+        <ModalFooter>
+          <button
+            id="buttonchanges"
+            className="float-right mb-2"
+            onClick={() => {
+              histroy.push("/SuperUser");
+            }}
+          >
+            OK
+          </button>
+        </ModalFooter>
       </Modal>
     </Fragment>
   );
