7917599f8da6fe9259ae0de36ae75f4a4e3f7ef6
Shreya <dev@gmail.com>
2023-04-15 16:18:14 +0530

    super admin side tenant filter ui fixing done by shreya

--- client/src/components/dashboard/TenantFilters.js
+++ client/src/components/dashboard/TenantFilters.js
@@ -170,7 +170,7 @@ const TenantFilters = ({
       {logUser.usergroup === "Super Admin" ? (
         //super admin filterrr
         <Fragment>
-          <div className="container_align top_menu col-sm-12 responsiveDiv ">
+          <div className="container_align top_menu col-sm-12 responsiveDiv  ">
             {/* <div className="row pb-2  ml-2 responsiveDiv  bg-success "> */}
             <div className="col-lg-12 col-md-1 col-sm-1 col-1 text-center tenantfilter">
               {/* this is for textbox below image for showing the total count of Renewal */}
@@ -184,7 +184,7 @@ const TenantFilters = ({
                     title="Previous Year Renewal Count"
                     to="/Organization-filter"
                    // style={{ minWidth: "100px" }}
-                    className="top_box"
+                    className="top_box "
                     onClick={() => oldExpCountFetchOrg()}
                   >
                     {" "}
